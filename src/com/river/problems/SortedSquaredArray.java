b3b075af068c932e293068d6aaad55c816c07937
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-04-11 23:17:44 +0800

    initial commit

---
+++ src/com/river/problems/SortedSquaredArray.java
@@ -0,0 +1,25 @@
+package com.river.problems;
+
+import java.util.Arrays;
+
+public class SortedSquaredArray {
+
+    public int[] sortedSquaredArray(int[] array) {
+        int j = 0, k = array.length - 1, l = array.length - 1;
+        int[] output = new int[array.length];
+        while (j <= k) {
+            int currentMax = Math.max(Math.abs(array[j]), Math.abs(array[k]));
+            if (currentMax == Math.abs(array[j]))
+                j++;
+            else
+                k--;
+            output[l--] = currentMax * currentMax;
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        SortedSquaredArray s = new SortedSquaredArray();
+        System.out.printf("1. %s\n", Arrays.toString(s.sortedSquaredArray(new int[] {1, 2, 3, 5, 6, 8, 9}))); // 1, 4, 9, 25, 36, 64, 81
+    }
+}
---
+++ src/com/river/problems/TwoNumberSum.java
@@ -0,0 +1,25 @@
+package com.river.problems;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoNumberSum {
+
+    public static int[] twoNumberSum(int[] array, int targetSum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < array.length; i++) {
+            if (map.containsKey(array[i])) {
+                return new int[] { array[i], map.get(array[i]) };
+            }
+            int diff = targetSum - array[i];
+            map.put(diff, array[i]);
+        }
+
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("1. %s\n", Arrays.toString(twoNumberSum(new int[] {3, 5, -4, 8, 11, 1, -1, 6}, 10))); // [-1, 11]
+    }
+}
