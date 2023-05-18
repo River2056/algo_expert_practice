72faba181f737892c93c0d88223bc5d6049eee29
river2056 <chen0625tung@gmail.com>
2023-05-18 23:04:24 +0800

    + MoveElementToTheEnd + SmallestDifference

---
+++ src/com/river/problems/medium/MoveElementToTheEnd.java
@@ -0,0 +1,30 @@
+package com.river.problems.medium;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MoveElementToTheEnd {
+    public static List<Integer> moveElementToEnd(List<Integer> array, int toMove) {
+        int i = 0, j = array.size() - 1;
+        while (i < j) {
+            while (i < j && array.get(j) == toMove) j--;
+            if (array.get(i) == toMove) {
+                int temp = array.get(i);
+                array.set(i, array.get(j));
+                array.set(j, temp);
+                j--;
+            }
+            i++;
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("1. %s\n", moveElementToEnd(Arrays.asList(2, 1, 2, 2, 2, 3, 4, 2), 2));
+        System.out.printf("2. %s\n", moveElementToEnd(Arrays.asList(2, 1, 2, 2, 2, 3, 4, 2), 2));
+        System.out.printf("3. %s\n", moveElementToEnd(Arrays.asList(3, 3, 3, 3, 3), 3));
+        System.out.printf("4. %s\n", moveElementToEnd(Arrays.asList(5, 1, 2, 5, 5, 3, 4, 6, 7, 5, 8, 9, 10, 11, 5, 5, 12), 5));
+        System.out.printf("5. %s\n", moveElementToEnd(Arrays.asList(1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 5, 5, 5, 5, 5, 5), 5));
+    }
+}
---
+++ src/com/river/problems/medium/SmallestDifference.java
@@ -0,0 +1,36 @@
+package com.river.problems.medium;
+
+import java.util.Arrays;
+
+public class SmallestDifference {
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+        int i = 0, j = 0;
+        int[] result = new int[2];
+        int min = Integer.MAX_VALUE;
+        while (i < arrayOne.length && j < arrayTwo.length) {
+            int diff = Math.abs(arrayOne[i] - arrayTwo[j]);
+            if (diff < min) {
+                min = diff;
+                result[0] = arrayOne[i];
+                result[1] = arrayTwo[j];
+            }
+
+            if (arrayOne[i] < arrayTwo[j])
+                i++;
+            else
+                j++;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("1. %s\n", Arrays.toString(smallestDifference(
+            new int[] {-1, 5, 10, 20, 28, 3},
+            new int[] {26, 134, 135, 15, 17}
+        )));
+         
+    }
+}
