23d039585d451af46e3adbdc81edf40daadd7f24
river2056 <chen0625tung@gmail.com>
2023-07-11 19:16:38 +0800

    + SweetAndSavory added extra solutions

--- app/src/main/java/algo_expert_practice/problems/medium/SweetAndSavory.java
+++ app/src/main/java/algo_expert_practice/problems/medium/SweetAndSavory.java
@@ -1,6 +1,7 @@
 package algo_expert_practice.problems.medium;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 /**
  * SweetAndSavory
@@ -27,6 +28,32 @@ public class SweetAndSavory {
                     result[1] = dishes[right];
                 }
                 left++;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] sweetAndSavoryAttempt2(int[] dishes, int target) {
+        Integer[] sweetDishes = Arrays.stream(dishes).boxed().filter(i -> i < 0).sorted((a, b) -> b - a).toArray(Integer[]::new);
+        Integer[] savoryDishes = Arrays.stream(dishes).boxed().filter(i -> i > 0).sorted().toArray(Integer[]::new);
+        int i = 0;
+        int j = 0;
+        int[] result = new int[] {0, 0};
+
+        int max = Integer.MAX_VALUE;
+        while (i < sweetDishes.length && j < savoryDishes.length) {
+            int combine = sweetDishes[i] + savoryDishes[j];
+            if (combine <= target) {
+                int diff = target - combine;
+                if (diff < max) {
+                    max = diff;
+                    result[0] = sweetDishes[i];
+                    result[1] = savoryDishes[j];
+                }
+                j++;
+            } else {
+                i++;
             }
         }
 
--- app/src/test/java/algo_expert_practice/medium/SweetAndSavoryTest.java
+++ app/src/test/java/algo_expert_practice/medium/SweetAndSavoryTest.java
@@ -26,4 +26,13 @@ class SweetAndSavoryTest {
         assertArrayEquals(new int[] {-25, 5}, sweetAndSavory.sweetAndSavory(new int[] {2, 5, -4, -7, 12, 100, -25}, -20));
         assertArrayEquals(new int[] {-102, 102}, sweetAndSavory.sweetAndSavory(new int[] {17, 37, 12, -102, 53, 49, -90, 102, 49, 16, 52}, 11));
     }
+
+
+    @Test
+    void test2() {
+        assertArrayEquals(new int[] {-3, 7}, sweetAndSavory.sweetAndSavoryAttempt2(new int[] {-3, -5, 1, 7}, 8));
+        assertArrayEquals(new int[] {0, 0}, sweetAndSavory.sweetAndSavoryAttempt2(new int[] {3, 5, 7, 2, 6, 8, 1}, 10));
+        assertArrayEquals(new int[] {-25, 5}, sweetAndSavory.sweetAndSavoryAttempt2(new int[] {2, 5, -4, -7, 12, 100, -25}, -20));
+        assertArrayEquals(new int[] {-102, 102}, sweetAndSavory.sweetAndSavoryAttempt2(new int[] {17, 37, 12, -102, 53, 49, -90, 102, 49, 16, 52}, 11));
+    }
 }
