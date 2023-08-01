e89ecc6dc010ac447eb4a23c670a6e6259652468
river2056 <chen0625tung@gmail.com>
2023-08-01 20:42:35 +0800

    + MergeBinaryTrees added recursive and iterative solution

--- app/src/main/java/algo_expert_practice/problems/medium/MergeBinaryTrees.java
+++ app/src/main/java/algo_expert_practice/problems/medium/MergeBinaryTrees.java
@@ -1,7 +1,10 @@
 package algo_expert_practice.problems.medium;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * MergeBinaryTrees
@@ -19,7 +22,7 @@ public class MergeBinaryTrees {
         }
     }
 
-    public BinaryTree mergeBinaryTrees(BinaryTree tree1, BinaryTree tree2) {
+    public BinaryTree mergeBinaryTreesSelf(BinaryTree tree1, BinaryTree tree2) {
         dfs(tree1, tree2, null, -1);
         return tree1;
     }
@@ -62,6 +65,53 @@ public class MergeBinaryTrees {
         printTree(tree.right, res);
     }
 
+    public BinaryTree mergeBinaryTreesRecursive(BinaryTree tree1, BinaryTree tree2) {
+        if (tree1 == null)
+            return tree2;
+        if (tree2 == null)
+            return tree1;
+
+        tree1.value += tree2.value;
+        tree1.left = mergeBinaryTreesRecursive(tree1.left, tree2.left);
+        tree1.right = mergeBinaryTreesRecursive(tree1.right, tree2.right);
+        return tree1;
+    }
+
+    public BinaryTree mergeBinaryTrees(BinaryTree tree1, BinaryTree tree2) {
+        if (tree1 == null)
+            return tree2;
+
+        Deque<Optional<BinaryTree>> tree1Stack = new ArrayDeque<>();
+        tree1Stack.add(Optional.ofNullable(tree1));
+        Deque<Optional<BinaryTree>> tree2Stack = new ArrayDeque<>();
+        tree2Stack.add(Optional.ofNullable(tree2));
+
+        while (!tree1Stack.isEmpty()) {
+            BinaryTree tree1Node = tree1Stack.removeLast().orElse(null);
+            BinaryTree tree2Node = tree2Stack.removeLast().orElse(null);
+
+            if (tree2Node == null)
+                continue;
+
+            tree1Node.value += tree2Node.value;
+            if (tree1Node.left == null)
+                tree1Node.left = tree2Node.left;
+            else {
+                tree1Stack.add(Optional.ofNullable(tree1Node.left));
+                tree2Stack.add(Optional.ofNullable(tree2Node.left));
+            }
+
+            if (tree1Node.right == null)
+                tree1Node.right = tree2Node.right;
+            else {
+                tree1Stack.add(Optional.ofNullable(tree1Node.right));
+                tree2Stack.add(Optional.ofNullable(tree2Node.right));
+            }
+        }
+
+        return tree1;
+    }
+
     public static void main(String[] args) {
         MergeBinaryTrees m = new MergeBinaryTrees();
         BinaryTree a = new BinaryTree(1);
@@ -83,5 +133,6 @@ public class MergeBinaryTrees {
         List<Integer> res = new ArrayList<>();
         m.printTree(a, res);
         System.out.println(res);
+
     }
 }
